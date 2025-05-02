bef11bc9152997b4e4be4ce8aee1b5f4f8f85ca5
Shanthosh <shanthubolt@gmail.com>
2025-05-02 16:34:00 +0530

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='askdb',
-    version='0.1.0',
+    version='0.1.1',
     author='Shanthosh',
     description='A simple and efficient database for storing and querying data.',
     long_description=open('README.md').read(),
